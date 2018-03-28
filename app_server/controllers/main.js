42237738bfbea6452d29f5ef13b5fc3e7d464379
sergey <sergey>
2018-03-28 16:02:34 +0300

    second commit

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,3 +1,21 @@
+var mongoose = require('mongoose');
+var Ag = mongoose.model('Agency');
+
+var sendJSONresponse = function(res, status, content) {
+  res.status(status);
+  res.json(content);
+};
+
 module.exports.index = function(req, res) {
+	//var allCity = Ag.find();
+
+	Ag.find({}, function(err, docs){
+    //mongoose.disconnect();
+     
+    if(err) return console.log(err);
+     
+    console.log(docs);
+	});
+
 	res.render('index', { title: "Tmp Express!!!" })
 }
